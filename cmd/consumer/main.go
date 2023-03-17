e3058b841f3e877ea86f23e7bd85a535dd73433d
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-03-17 08:23:26 -0300

    adicionando consumer kafka

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -8,7 +8,9 @@ import (
 	"github.com/AdrianeRibeiro/GoIntensivo/internal/infra/database"
 	"github.com/AdrianeRibeiro/GoIntensivo/internal/usecase"
 	"github.com/AdrianeRibeiro/GoIntensivo/pkg/kafka"
+	"github.com/AdrianeRibeiro/GoIntensivo/pkg/rabbitmq"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	amqp "github.com/rabbitmq/amqp091-go"
 
 	// sqlite3 driver
 	_ "github.com/mattn/go-sqlite3"
@@ -34,7 +36,17 @@ func main() {
 	fmt.Println("kafka consumer has started")
 
 	go kafka.Consume(topics, servers, msgChanKafka)
-	kafkaWorker(msgChanKafka, usecase)
+	go kafkaWorker(msgChanKafka, usecase)
+
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+
+	defer ch.Close()
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgRabbitmqChannel)
+	rabbitmqWorker(msgRabbitmqChannel, usecase)
 }
 
 func kafkaWorker(msgChan chan *ckafka.Message, uc usecase.CalculateFinalPrice) {
@@ -53,6 +65,27 @@ func kafkaWorker(msgChan chan *ckafka.Message, uc usecase.CalculateFinalPrice) {
 			panic(err)
 		}
 
+		fmt.Printf("kafka has processed order %s \n", outputDto.ID)
+	}
+}
+
+func rabbitmqWorker(msgChan chan amqp.Delivery, uc usecase.CalculateFinalPrice) {
+	fmt.Println("Rabbitmq worker has started")
+
+	for msg := range msgChan {
+		var OrderInputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &OrderInputDTO)
+
+		if err != nil {
+			panic(err)
+		}
+
+		outputDto, err := uc.Execute(OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		msg.Ack(false)
 		fmt.Printf("kafka has processed order %s \n", outputDto.ID)
 	}
 }
--- readme.md
+++ readme.md
@@ -10,3 +10,13 @@ subir docker
 entrar no bash
 http://localhost:9021
 go run cmd/consumer/main.go
+
+sudo docker stop $(sudo docker ps -q)
+sudo docker-compose up -d
+sudo docker ps
+sudo docker-compose exec goapp bash
+go mod tidy
+go run cmd/consumer/main.go
+/go/goapp/cmd/consumer# go build -o consumer
+http://localhost:15672/#/connections
+http://localhost:9021/clusters
